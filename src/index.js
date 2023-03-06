1087b72d53af971ff04c0d98f1ba1e2db3066a80
JJ van Rhyn <jjvrhyn@gmail.com>
2023-03-06 13:43:29 +0200

    play  with myModule

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import _ from 'lodash';
 
+//import myName function
+
+import myName from './myName';
+
+//import myModule
+
+import {functionOne, functionTwo} from './myModule';
+
 function component() {
-    const element = document.createElement('div');
+    let element = document.createElement('div');
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    element.textContent = myName('JJ v Rhyn');
+  
+    return element;
+  }
+
+  function componentTwo() {
+    let element = document.createElement('div');
+
+    element.textContent = functionOne;
   
     return element;
   }
   
   document.body.appendChild(component());
+
+  document.body.appendChild(componentTwo());
